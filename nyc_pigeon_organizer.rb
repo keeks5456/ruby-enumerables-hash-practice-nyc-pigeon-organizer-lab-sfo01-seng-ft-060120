b1694b676d988d7d0961eda4e183957b495e3e0d
Akiko Green <akikogreen08@gmail.com>
2020-05-03 23:53:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_names = {}
+data.each do |color_gender_live, info|
+  info.each do |keys, names|
+    names.each do |name|
+      if pigeon_names[name] == nil 
+        pigeon_names[name] = {}
+        binding.pry
+    end 
+    if pigeon_names[name][color_gender_live] == nil
+      pigeon_names[name][color_gender_live] = []
+      end
+    pigeon_names[name][color_gender_live].push(keys.to_s)
+    end
+    end
+  end
+  pigeon_names
 end
+
+
+# first off we create a new hash to store our data in.
+# is our data hash we must iterate over the :color, :gender: and :lives along with their values
