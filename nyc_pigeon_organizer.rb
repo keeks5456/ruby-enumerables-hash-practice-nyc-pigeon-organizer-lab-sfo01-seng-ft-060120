c52b087decf638d9337e9ec1b074f817b75670d7
Akiko Green <akikogreen08@gmail.com>
2020-05-04 00:05:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,4 +21,9 @@ end
 
 # first off we create a new hash to store our data in.
 # is our data hash we must iterate over the :color, :gender: and :lives -- i.e (color_gender_live); along with their key/value pairs i.e(info)
-#we then want to iterate over our info(keys) and find the names(name) inside of them
+#we then want to iterate over our info(keys) and find the array of igeon names(names) inside of them.
+# we then want to iterate through the names(name)  because we will then be able to grab each pigeon name individually.
+
+# then we ask --> if the there are no pigeon names in our empty hash, we must return an empty hash with each name of that pigeon creating their own empty hashes
+# then we ask --> if there are no pigeon names key and the color/gender/live key for our empty hash, we must assign and return it as an empty array.
+# lastly, we want to append to our pigeon_name[names][color_gender_live] its values (the info)
