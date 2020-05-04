0c21dbfa16e54cc79b5b9fcc72c22614886b8a36
Akiko Green <akikogreen08@gmail.com>
2020-05-04 00:07:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -26,4 +26,6 @@ end
 
 # then we ask --> if the there are no pigeon names in our empty hash, we must return an empty hash with each name of that pigeon creating their own empty hashes
 # then we ask --> if there are no pigeon names key and the color/gender/live key for our empty hash, we must assign and return it as an empty array.
-# lastly, we want to append to our pigeon_name[names][color_gender_live] its values (the info)
+# lastly, we want to append to our pigeon_name[names][color_gender_live] its values (the info) into the arrays which are now converted strings
+
+# resulting in returning the new pigeon hash
